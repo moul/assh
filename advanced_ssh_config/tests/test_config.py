f7518ccec94e9e37fb5f5b20a500b0ac72b6dd32
Manfred Touron <m@42.am>
2013-09-28 18:48:09 +0200

    added tests

--- advanced_ssh_config/tests/test_config.py
+++ advanced_ssh_config/tests/test_config.py
@@ -105,7 +105,45 @@ Includes = {0}/include-1 {0}/include-1
     def test_get_simple(self):
         contents = """
 [hosta]
-hostmame = 1.2.3.4
+hostname = 1.2.3.4
+""".format(PREFIX)
+        write_config(contents)
+        config = Config([DEFAULT_CONFIG])
+        self.assertEquals(config.get('Hostname', 'hosta'), '1.2.3.4')
+        self.assertEquals(config.get('hostname', 'hosta'), '1.2.3.4')
+
+    def test_get_key_not_found(self):
+        contents = """
+[hosta]
+""".format(PREFIX)
+        write_config(contents)
+        config = Config([DEFAULT_CONFIG])
+        self.assertEquals(config.get('Hostname', 'hosta'), None)
+        self.assertEquals(config.get('Hostname', 'hosta', 'localhost'), 'localhost')
+
+    def test_get_host_not_found(self):
+        contents = """
+[default]
+port = 22
+""".format(PREFIX)
+        write_config(contents)
+        config = Config([DEFAULT_CONFIG])
+        self.assertEquals(config.get('Port', 'hosta'), '22')
+
+    def test_get_host_and_key_not_found(self):
+        write_config('')
+        config = Config([DEFAULT_CONFIG])
+        self.assertEquals(config.get('Port', 'hosta'), None)
+
+    def test_host_wildcard(self):
+        contents = """
+[aaa.*]
+port = 25
+
+[.*bbb]
+port = 24
+
+[ccc.*ddd]
 port = 23
 
 [default]
@@ -113,7 +151,14 @@ port = 22
 """.format(PREFIX)
         write_config(contents)
         config = Config([DEFAULT_CONFIG])
-        print()
-        print('-' * 80)
-        print(config.get('hostname', 'hosta', 'def'))
-        print('-' * 80)
+        self.assertEquals(config.get('Port', 'aaa'), '25')
+        self.assertEquals(config.get('Port', 'aaa42'), '25')
+        self.assertEquals(config.get('Port', '42aaa'), '22')
+
+        self.assertEquals(config.get('Port', 'bbb'), '24')
+        self.assertEquals(config.get('Port', 'bbb42'), '24')  # strange
+        self.assertEquals(config.get('Port', '42bbb'), '24')
+
+        self.assertEquals(config.get('Port', 'cccddd'), '23')
+        self.assertEquals(config.get('Port', 'ccc42ddd'), '23')
+
