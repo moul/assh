f851b6746b5b9fc1447e37b2e2bd298070833e3d
Manfred Touron <m@42.am>
2015-03-23 10:07:27 +0100

    Updated build scripts

--- setup.py
+++ setup.py
@@ -8,19 +8,25 @@ import imp
 from setuptools import setup, find_packages
 
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(
+    os.path.abspath(os.path.dirname(__file__))
+)
 
 
 MODULE_NAME = 'advanced_ssh_config'
 MODULE = imp.load_module(MODULE_NAME, *imp.find_module(MODULE_NAME))
 
+DEPENDENCIES = [
+]
 
-description = """
-    Extended ssh configuration (.ssh/config) with regex, aliases, gateways,
-    includes, command execution, variable expansion...
+TEST_DEPENDENCIES = [
+    'coverage',
+    'mock',
+    'nose',
+    'pep8',
+]
 
-    An alternative ssh configuration file with extended options and features
-"""
+description = "Add some magic to SSH and .ssh/config"
 long_description = description
 if os.path.exists('README.rst'):
     long_description = open('README.rst').read()
@@ -37,26 +43,27 @@ setup(
     download_url='https://github.com/moul/advanced-ssh-config/archive/master.zip',
     packages=find_packages(),
     package_dir={'advanced_ssh_config': 'advanced_ssh_config'},
-    tests_require=['mock', 'nose', 'coverage', 'pep8'],
+    install_requires=DEPENDENCIES,
+    tests_require=TEST_DEPENDENCIES,
     extras_require={
         'process_inspection': ['psutil'],
         'password': ['pexpect'],
         'release': ['PyInstaller'],
     },
-    test_suite=MODULE_NAME + '.tests',
+    test_suite='{}.tests'.format(MODULE_NAME),
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
         'Operating System :: MacOS',
+        'Operating System :: POSIX',
         'Operating System :: Unix',
         'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet',
-        'Topic :: System :: Systems Administration',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Shells',
+        'Topic :: System :: Systems Administration',
     ],
     license='MIT',
     entry_points={
