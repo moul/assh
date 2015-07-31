c60e42b062001029a8ea10e7dec12c9325d7497d
Manfred Touron <m@42.am>
2015-07-31 22:43:55 +0200

    Golang branch will begin at version 2.0, so we can keep the same GitHub repository and old releases

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION should be updated by hand at each release
-	VERSION = "0.1.0-dev"
+	VERSION = "2.0.0-dev"
 
 	// GITCOMMIT will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
