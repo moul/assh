ac4c4fb0cd17398ce07a301b7bb80f281c4a8936
Manfred Touron <m@42.am>
2015-08-02 18:05:58 +0200

    Added Host.Gateway field

--- pkg/config/host.go
+++ pkg/config/host.go
@@ -2,9 +2,10 @@ package config
 
 // Host defines the configuration flags of a host
 type Host struct {
-	Host string `yaml:"host,omitempty,flow" json:"host,omitempty"`
-	User string `yaml:"user,omitempty,flow" json:"user,omitempty"`
-	Port uint   `yaml:"port,omitempty,flow" json:"port,omitempty"`
+	Host    string `yaml:"host,omitempty,flow"    json:"host,omitempty"`
+	User    string `yaml:"user,omitempty,flow"    json:"user,omitempty"`
+	Port    uint   `yaml:"port,omitempty,flow"    json:"port,omitempty"`
+	Gateway string `yaml:"gateway,omitempty,flow" json:"gateway,omitempty"`
 }
 
 // ApplyDefaults ensures a Host is valid by filling the missing fields with defaults
@@ -17,6 +18,9 @@ func (h *Host) ApplyDefaults(defaults Host) {
 	}
 	if h.Port == 0 {
 		h.Port = defaults.Port
+	}
+	if h.Gateway == "" {
+		h.Gateway = defaults.Gateway
 	}
 
 	// Extra defaults
