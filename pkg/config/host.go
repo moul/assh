7c20f4e7f636a30d83ae413589222c5938731b6b
Manfred Touron <m@42.am>
2015-08-05 20:58:20 +0200

    Initial version of gateways

--- pkg/config/host.go
+++ pkg/config/host.go
@@ -2,9 +2,10 @@ package config
 
 // Host defines the configuration flags of a host
 type Host struct {
-	Host string `yaml:"host,omitempty,flow" json:"host,omitempty"`
-	User string `yaml:"user,omitempty,flow" json:"user,omitempty"`
-	Port uint   `yaml:"port,omitempty,flow" json:"port,omitempty"`
+	Host     string   `yaml:"host,omitempty,flow" json:"host,omitempty"`
+	User     string   `yaml:"user,omitempty,flow" json:"user,omitempty"`
+	Port     uint     `yaml:"port,omitempty,flow" json:"port,omitempty"`
+	Gateways []string `yaml:"gateways,omitempty,flow" json:"gateways,omitempty"`
 }
 
 // ApplyDefaults ensures a Host is valid by filling the missing fields with defaults
@@ -17,6 +18,9 @@ func (h *Host) ApplyDefaults(defaults Host) {
 	}
 	if h.Port == 0 {
 		h.Port = defaults.Port
+	}
+	if len(h.Gateways) == 0 {
+		h.Gateways = defaults.Gateways
 	}
 
 	// Extra defaults
