b27ee59eb68a7681f0ac3f3991002a1d31b4de7e
Manfred Touron <m@42.am>
2015-08-10 22:55:36 +0200

    Support of Host.ResolveCommand

--- pkg/config/host.go
+++ pkg/config/host.go
@@ -2,13 +2,14 @@ package config
 
 // Host defines the configuration flags of a host
 type Host struct {
-	Name         string   `yaml:-`
-	Host         string   `yaml:"host,omitempty,flow" json:"host,omitempty"`
-	User         string   `yaml:"user,omitempty,flow" json:"user,omitempty"`
-	Port         uint     `yaml:"port,omitempty,flow" json:"port,omitempty"`
-	ProxyCommand string   `yaml:"proxycommand,omitempty,flow" json:"proxycommand,omitempty"`
-	Gateways     []string `yaml:"gateways,omitempty,flow" json:"gateways,omitempty"`
-	Resolve      string   `yaml:"resolve,omitempty,flow" json:"resolve,omitempty"`
+	Name               string   `yaml:-`
+	Host               string   `yaml:"host,omitempty,flow" json:"host,omitempty"`
+	User               string   `yaml:"user,omitempty,flow" json:"user,omitempty"`
+	Port               uint     `yaml:"port,omitempty,flow" json:"port,omitempty"`
+	ProxyCommand       string   `yaml:"proxycommand,omitempty,flow" json:"proxycommand,omitempty"`
+	Gateways           []string `yaml:"gateways,omitempty,flow" json:"gateways,omitempty"`
+	ResolveNameservers []string `yaml:"resolve_nameservers,omitempty,flow" json:"resolve_nameservers,omitempty"`
+	ResolveCommand     string   `yaml:"resolve_command,omitempty,flow" json:"resolve_command,omitempty"`
 }
 
 // ApplyDefaults ensures a Host is valid by filling the missing fields with defaults
@@ -28,8 +29,11 @@ func (h *Host) ApplyDefaults(defaults Host) {
 	if h.ProxyCommand == "" {
 		h.ProxyCommand = defaults.ProxyCommand
 	}
-	if h.Resolve == "" {
-		h.Resolve = defaults.Resolve
+	if len(h.ResolveNameservers) == 0 {
+		h.ResolveNameservers = defaults.ResolveNameservers
+	}
+	if h.ResolveCommand == "" {
+		h.ResolveCommand = defaults.ResolveCommand
 	}
 
 	// Extra defaults
