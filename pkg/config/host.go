1ee1d92d93d7917c03143657082d23d56e43c920
Manfred Touron <m@42.am>
2015-08-10 20:04:17 +0200

    Support of custom proxy command

--- pkg/config/host.go
+++ pkg/config/host.go
@@ -2,12 +2,13 @@ package config
 
 // Host defines the configuration flags of a host
 type Host struct {
-	Name     string   `yaml:-`
-	Host     string   `yaml:"host,omitempty,flow" json:"host,omitempty"`
-	User     string   `yaml:"user,omitempty,flow" json:"user,omitempty"`
-	Port     uint     `yaml:"port,omitempty,flow" json:"port,omitempty"`
-	Gateways []string `yaml:"gateways,omitempty,flow" json:"gateways,omitempty"`
-	Resolve  string   `yaml:"resolve,omitempty,flow" json:"resolve,omitempty"`
+	Name         string   `yaml:-`
+	Host         string   `yaml:"host,omitempty,flow" json:"host,omitempty"`
+	User         string   `yaml:"user,omitempty,flow" json:"user,omitempty"`
+	Port         uint     `yaml:"port,omitempty,flow" json:"port,omitempty"`
+	ProxyCommand string   `yaml:"proxycommand,omitempty,flow" json:"proxycommand,omitempty"`
+	Gateways     []string `yaml:"gateways,omitempty,flow" json:"gateways,omitempty"`
+	Resolve      string   `yaml:"resolve,omitempty,flow" json:"resolve,omitempty"`
 }
 
 // ApplyDefaults ensures a Host is valid by filling the missing fields with defaults
@@ -23,6 +24,12 @@ func (h *Host) ApplyDefaults(defaults Host) {
 	}
 	if len(h.Gateways) == 0 {
 		h.Gateways = defaults.Gateways
+	}
+	if h.ProxyCommand == "" {
+		h.ProxyCommand = defaults.ProxyCommand
+	}
+	if h.Resolve == "" {
+		h.Resolve = defaults.Resolve
 	}
 
 	// Extra defaults
