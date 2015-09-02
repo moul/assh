f2c3accdfa91714056b3855c585216991ddf8d3d
Manfred Touron <m@42.am>
2015-09-02 11:10:28 +0200

    Taking greatest log level

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,6 +1,12 @@
 package logger
 
-import "github.com/moul/advanced-ssh-config/vendor/github.com/Sirupsen/logrus"
+import (
+	"os"
+	"strings"
+
+	"github.com/moul/advanced-ssh-config/vendor/github.com/Sirupsen/logrus"
+	"github.com/shirou/gopsutil/process"
+)
 
 var Logger = logrus.New()
 
@@ -8,4 +14,44 @@ func LoggerSetLevel(level logrus.Level) {
 	// Logger.mu.Lock()
 	// defer Logger.mu.Unlock()
 	Logger.Level = level
+}
+
+type LoggerOptions struct {
+	Level         logrus.Level
+	InspectParent bool
+}
+
+func SetupLogging(options LoggerOptions) {
+	level := options.Level
+
+	if options.InspectParent {
+		parentLevel, err := GetLoggingLevelByInspectingParent()
+		if err != nil {
+			Logger.Debugf("Failed to inspect parent process: %v", err)
+		} else if parentLevel > level {
+			level = parentLevel
+		}
+	}
+
+	LoggerSetLevel(level)
+}
+
+func GetLoggingLevelByInspectingParent() (logrus.Level, error) {
+	ppid := os.Getppid()
+	process, err := process.NewProcess(int32(ppid))
+	if err != nil {
+		return logrus.WarnLevel, err
+	}
+
+	cmdline, err := process.Cmdline()
+	if err != nil {
+		return logrus.WarnLevel, err
+	}
+
+	if strings.Contains(cmdline, "-vv") {
+		return logrus.DebugLevel, nil
+	} else if strings.Contains(cmdline, "-v") {
+		return logrus.InfoLevel, nil
+	}
+	return logrus.WarnLevel, nil
 }
