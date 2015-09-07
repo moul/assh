efaff61d1175127c02afa703b001771ad30e00b9
Manfred Touron <m@42.am>
2015-09-07 15:26:24 +0200

    v2.0.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version should be updated by hand at each release
-	Version = "2.0.0-dev"
+	Version = "2.0.0"
 
 	// GitCommit will be overwritten automatically by the build system
 	GitCommit = "HEAD"
