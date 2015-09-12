ba8dbbcf1df50cd53258255d633ac3c20c3f320b
Manfred Touron <m@42.am>
2015-09-12 16:55:38 +0200

    Initial version of flexyaml library (#51)

---
+++ pkg/flexyaml/flexyaml.go
@@ -0,0 +1,29 @@
+package flexyaml
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/moul/advanced-ssh-config/vendor/gopkg.in/yaml.v2"
+)
+
+func MakeFlexible(in []byte) ([]byte, error) {
+	lines := []string{}
+	for _, line := range strings.Split(string(in), "\n") {
+		if strings.Contains(line, ":") {
+			parts := strings.SplitN(line, ":", 2)
+			line = fmt.Sprintf("%s:%s", strings.ToLower(parts[0]), parts[1])
+		}
+		lines = append(lines, line)
+	}
+	return []byte(strings.Join(lines, "\n")), nil
+}
+
+func Unmarshal(in []byte, out interface{}) (err error) {
+	flex, err := MakeFlexible(in)
+	if err != nil {
+		return err
+	}
+
+	return yaml.Unmarshal(flex, out)
+}
