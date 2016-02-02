bd8fcc00b7c425031060944fc65a0d485c51578e
Manfred Touron <m@42.am>
2016-02-02 19:39:48 +0100

    Anonymize 'assh info'

--- pkg/config/utils.go
+++ pkg/config/utils.go
@@ -6,15 +6,23 @@ import (
 	"strings"
 )
 
+// GetHomeDir returns '~' as a path
+func GetHomeDir() string {
+	if homeDir := os.Getenv("HOME"); homeDir != "" {
+		return homeDir
+	}
+	if homeDir := os.Getenv("USERPROFILE"); homeDir != "" {
+		return homeDir
+	}
+	return ""
+}
+
 func expandUser(path string) (string, error) {
 	// Expand variables
 	path = os.ExpandEnv(path)
 
 	if path[:2] == "~/" {
-		homeDir := os.Getenv("HOME") // *nix
-		if homeDir == "" {           // Windows
-			homeDir = os.Getenv("USERPROFILE")
-		}
+		homeDir := GetHomeDir()
 		if homeDir == "" {
 			return "", errors.New("user home directory not found")
 		}
