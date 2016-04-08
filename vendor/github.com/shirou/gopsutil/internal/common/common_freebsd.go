278ed5d715b17824fb039e6ae3849a26696e11ee
Manfred Touron <m@42.am>
2016-04-09 00:08:12 +0200

    make godep-save

--- vendor/github.com/shirou/gopsutil/internal/common/common_freebsd.go
+++ vendor/github.com/shirou/gopsutil/internal/common/common_freebsd.go
@@ -3,6 +3,7 @@
 package common
 
 import (
+	"os"
 	"os/exec"
 	"strings"
 	"syscall"
@@ -10,7 +11,15 @@ import (
 )
 
 func DoSysctrl(mib string) ([]string, error) {
-	out, err := exec.Command("/sbin/sysctl", "-n", mib).Output()
+	err := os.Setenv("LC_ALL", "C")
+	if err != nil {
+		return []string{}, err
+	}
+	sysctl, err := exec.LookPath("/sbin/sysctl")
+	if err != nil {
+		return []string{}, err
+	}
+	out, err := exec.Command(sysctl, "-n", mib).Output()
 	if err != nil {
 		return []string{}, err
 	}
