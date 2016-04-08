5d4229b9200958899d184c1c24190d8eed5e6500
Manfred Touron <m@42.am>
2016-04-09 01:03:35 +0200

    Add Golang comments

--- pkg/config/ssh_flags.go
+++ pkg/config/ssh_flags.go
@@ -2,8 +2,13 @@ package config
 
 import "github.com/codegangsta/cli"
 
+// SSHFlags is built at init(), it contains codegangsta/cli string & bool flags for SSH
 var SSHFlags = []cli.Flag{}
+
+// SSHBoolFlags contains list of available SSH boolean options
 var SSHBoolFlags = []string{"1", "2", "4", "6", "A", "a", "C", "f", "G", "g", "K", "k", "M", "N", "n", "q", "s", "T", "t", "V", "v", "X", "x", "Y", "y"}
+
+// SSHStringFlags contains list of available SSH string options
 var SSHStringFlags = []string{"b", "c", "D", "E", "e", "F", "I", "i", "L", "l", "m", "O", "o", "p", "Q", "R", "S", "W", "w"}
 
 func init() {
