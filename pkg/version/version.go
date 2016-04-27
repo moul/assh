5eaaf15f2b002e7ade6520b2df2911f2f33987de
Manfred Touron <m@42.am>
2016-04-27 11:47:35 +0200

    v2.3.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,7 +5,7 @@ var GITCOMMIT string
 
 func init() {
 	// Version should be updated by hand at each release
-	VERSION = "2.2.0-dev"
+	VERSION = "2.3.0"
 	// GitCommit will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
 }
