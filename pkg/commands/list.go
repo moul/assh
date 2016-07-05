4e5d3cfaf76bfd1e8b578d9f2eced8be16bbbf38
Manfred Touron <m@42.am>
2016-07-05 10:09:22 +0200

    Initial version of listing

---
+++ pkg/commands/list.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/mgutz/ansi"
+	"github.com/moul/advanced-ssh-config/pkg/config"
+	// . "github.com/moul/advanced-ssh-config/pkg/logger"
+)
+
+func cmdList(c *cli.Context) {
+	conf, err := config.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	// ansi coloring
+	colorize := func(input string) string { return input }
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		colorize = ansi.ColorFunc("green+b+h")
+	}
+
+	for _, host := range conf.Hosts {
+		host.ApplyDefaults(&conf.Defaults)
+		fmt.Printf("    %s -> %s\n", colorize(host.Name()), host.Prototype())
+		fmt.Println()
+	}
+}
