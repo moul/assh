8587b7a57a80983b9b75be37f0b739a1f4e62a4b
Manfred Touron <m@42.am>
2016-07-06 11:21:15 +0200

    Add comments for godoc

--- pkg/config/option.go
+++ pkg/config/option.go
@@ -2,17 +2,20 @@ package config
 
 import "fmt"
 
+// Option is an host option
 type Option struct {
 	Name  string
 	Value string
 }
 
+// OptionList is a list of options
 type OptionsList []Option
 
 func (o *Option) String() string {
 	return fmt.Sprintf("%s=%s", o.Name, o.Value)
 }
 
+// ToStringList returns a list of string with the following format: `key=value`
 func (ol *OptionsList) ToStringList() []string {
 	list := []string{}
 	for _, opt := range *ol {
@@ -21,6 +24,7 @@ func (ol *OptionsList) ToStringList() []string {
 	return list
 }
 
+// Remove removes an option from the list based on its key
 func (ol *OptionsList) Remove(key string) {
 	for i, opt := range *ol {
 		if opt.Name == key {
