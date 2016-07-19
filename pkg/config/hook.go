1602aadf6ff39f7bd07ff31f3479831e6a11ed7a
Manfred Touron <m@42.am>
2016-07-19 09:38:33 +0200

    Comment exported classes

--- pkg/config/hook.go
+++ pkg/config/hook.go
@@ -2,11 +2,13 @@ package config
 
 import "encoding/json"
 
+// HostHooks represents a static list of Hooks
 type HostHooks struct {
 	OnConnect    Hooks `yaml:"onconnect,omitempty,flow" json:"OnConnect,omitempty"`
 	OnDisconnect Hooks `yaml:"ondisconnect,omitempty,flow" json:"OnDisconnect,omitempty"`
 }
 
+// Length returns the quantity of hooks of any type
 func (hh *HostHooks) Length() int {
 	if hh == nil {
 		return 0
@@ -20,6 +22,8 @@ func (hh *HostHooks) String() string {
 	return string(s)
 }
 
+// Hooks represents a slice of Hook
 type Hooks []Hook
 
+// Hook is a string
 type Hook string
