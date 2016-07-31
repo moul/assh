b09889c3ff7023506d2498d1ded1af44b9b331cf
Manfred Touron <m@42.am>
2016-07-31 17:05:17 +0200

    Add OnConnectError hook

--- pkg/config/hook.go
+++ pkg/config/hook.go
@@ -8,9 +8,10 @@ import (
 
 // HostHooks represents a static list of Hooks
 type HostHooks struct {
-	BeforeConnect hooks.Hooks `yaml:"beforeconnect,omitempty,flow" json:"BeforeConnect,omitempty"`
-	OnConnect     hooks.Hooks `yaml:"onconnect,omitempty,flow" json:"OnConnect,omitempty"`
-	OnDisconnect  hooks.Hooks `yaml:"ondisconnect,omitempty,flow" json:"OnDisconnect,omitempty"`
+	BeforeConnect  hooks.Hooks `yaml:"beforeconnect,omitempty,flow" json:"BeforeConnect,omitempty"`
+	OnConnect      hooks.Hooks `yaml:"onconnect,omitempty,flow" json:"OnConnect,omitempty"`
+	OnDisconnect   hooks.Hooks `yaml:"ondisconnect,omitempty,flow" json:"OnDisconnect,omitempty"`
+	OnConnectError hooks.Hooks `yaml:"onconnecterror,omitempty,flow" json:"OnConnectError,omitempty"`
 }
 
 // Length returns the quantity of hooks of any type
