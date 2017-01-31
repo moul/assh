4633f462bb1cd4a3ae90556aef0809ac68934680
Manfred Touron <m@42.am>
2017-01-31 22:37:01 +0100

    Switch to JSON input for the webapp

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -28,18 +28,24 @@ func server(c *cli.Context) error {
 	router.GET("/ping", func(c *gin.Context) { c.String(200, "pong") })
 	router.POST("/assh-to-ssh", func(c *gin.Context) {
 		var (
-			err        error
-			cfg        = config.New()
-			buffer     bytes.Buffer
-			asshConfig = c.PostForm("assh_config")
+			err    error
+			cfg    = config.New()
+			buffer bytes.Buffer
+			json   struct {
+				AsshConfig string `form:"assh_config" json:"assh_config"`
+			}
 		)
 
-		if asshConfig == "" {
+		if err = c.BindJSON(&json); err != nil {
+			goto serverEnd
+		}
+
+		if json.AsshConfig == "" {
 			err = fmt.Errorf("invalid input")
 			goto serverEnd
 		}
 
-		if err = cfg.LoadConfig(strings.NewReader(asshConfig)); err != nil {
+		if err = cfg.LoadConfig(strings.NewReader(json.AsshConfig)); err != nil {
 			goto serverEnd
 		}
 
@@ -54,8 +60,8 @@ func server(c *cli.Context) error {
 			})
 		} else {
 			c.JSON(200, gin.H{
-				"assh_config": asshConfig,
-				"ssh_config":  buffer.String(),
+				//"assh_config": json.AsshConfig,
+				"ssh_config": buffer.String(),
 			})
 		}
 	})
