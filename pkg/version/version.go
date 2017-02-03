2e953338c245b67dc2245dc98a1b3edbec71550b
Manfred Touron <m@42.am>
2017-02-03 16:58:08 +0100

    Post-release version bump

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version should be updated by hand at each release
-	VERSION = "2.6.0"
+	VERSION = "2.6.0+dev"
 	// GitCommit will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
 )
