08e4aedd64c9f04bdf9758120d11edf92a68604b
Manfred Touron <m@42.am>
2018-07-03 17:55:28 +0200

    v2.8.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION should be updated by hand at each release
-	VERSION = "2.7.0+dev"
+	VERSION = "2.8.0"
 	// GITCOMMIT will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
 )
