405f492c7a7f2d2030dc5462502032c9e626c951
Chanchal Kumar Ghosh <284846+chanchal1987@users.noreply.github.com>
2021-11-23 16:15:57 -0800

    Minor bug fixes in "sockets flush" command

--- pkg/commands/control-sockets.go
+++ pkg/commands/control-sockets.go
@@ -108,7 +108,10 @@ func runFlushSocketsCommand(_ *cobra.Command, _ []string) error {
 		for _, ps := range processes {
 			if cmdline, err := ps.CmdlineSlice(); err == nil && len(cmdline) > 0 && path.Base(cmdline[0]) == "assh" && cmdline[1] == "connect" {
 				cmd := exec.Command("ssh", "-O", "exit", cmdline[len(cmdline)-1]) // #nosec
-				if err := cmd.Run(); err != nil {
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+
+				if err := cmd.Run(); err == nil {
 					success++
 				}
 			}
