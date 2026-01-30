50f5bb755dc42ddffefad43f1cbbd691e8bf33c4
milkpirate <milkpirate@users.noreply.github.com>
2026-01-30 20:57:49 +0100

    fix: exec hook for git bash (#691)
    Signed-off-by: Paul Schroeder <milkpirate@users.noreply.github.com>
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -24,10 +24,10 @@ require (
 	github.com/spf13/cobra v1.10.2
 	github.com/spf13/pflag v1.0.10
 	github.com/spf13/viper v1.21.0
+	github.com/stretchr/testify v1.11.1
 	github.com/urfave/cli v1.22.17
 	go.uber.org/zap v1.24.0
-	golang.org/x/crypto v0.47.0
-	golang.org/x/net v0.49.0
+	golang.org/x/term v0.39.0
 	golang.org/x/text v0.33.0
 	golang.org/x/time v0.3.0
 )
@@ -40,6 +40,7 @@ require (
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deckarep/gosx-notifier v0.0.0-20180201035817-e127226297fb // indirect
 	github.com/docker/docker v25.0.13+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
@@ -68,6 +69,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/quic-go/qpack v0.6.0 // indirect
 	github.com/quic-go/quic-go v0.59.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
@@ -86,8 +88,10 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/arch v0.23.0 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
+	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
-	golang.org/x/term v0.39.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/hooks/driver_exec.go
+++ pkg/hooks/driver_exec.go
@@ -5,9 +5,21 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
+	"runtime"
 
 	"moul.io/assh/v2/pkg/templates"
+)
+
+var (
+	possibleShells = []string{
+		"/bin/sh", "/bin/bash", "/bin/zsh",
+		"/usr/bin/sh", "/usr/bin/bash", "/usr/bin/zsh",
+		"/usr/local/bin/sh", "/usr/local/bin/bash", "/usr/local/bin/zsh",
+
+		"C:\\Program Files\\Git\\bin\\bash.exe",
+		"C:\\Program Files\\Git\\bin\\sh.exe",
+		"C:\\Windows\\System32\\bash.exe",
+	}
 )
 
 // ExecDriver is a driver that execs some texts to the terminal
@@ -24,47 +36,67 @@ func NewExecDriver(line string) (ExecDriver, error) {
 
 // Run execs a line to the terminal
 func (d ExecDriver) Run(args RunArgs) error {
-	var buff bytes.Buffer
-	tmpl, err := templates.New(d.line + "\n")
-	if err != nil {
-		return err
-	}
-
-	if err := tmpl.Execute(&buff, args); err != nil {
-		return err
-	}
-
-	var (
-		availableShells = []string{
-			"/bin/sh", "/bin/bash", "/bin/zsh",
-			"/usr/bin/sh", "/usr/bin/bash", "/usr/bin/zsh",
-			"/usr/local/bin/sh", "/usr/local/bin/bash", "/usr/local/bin/zsh",
-		}
-		selectedShell = ""
-	)
-	for _, shell := range availableShells {
-		info, err := os.Stat(shell)
-		if err != nil {
-			continue
-		}
-		if info.Mode()&0111 != 0 {
-			selectedShell = shell
-			break
-		}
-	}
+	selectedShell := findAvailableShell(possibleShells)
 	if selectedShell == "" {
-		return fmt.Errorf("no available shell found. (tried %s)", strings.Join(availableShells, ", "))
+		return fmt.Errorf("no available shell found. (tried %s)", possibleShells)
 	}
 
-	cmd := exec.Command(selectedShell, "-c", buff.String()) // #nosec
-	cmd.Stdout = os.Stderr
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-	if err := cmd.Start(); err != nil {
+	command, err := renderCommand(d.line, args)
+	if err != nil {
+		return fmt.Errorf("failed to render command: %w", err)
+	}
+
+	proc := exec.Command(selectedShell, "-c", command) // #nosec
+
+	proc.Stdout = os.Stderr
+	proc.Stderr = os.Stderr
+	proc.Stdin = os.Stdin
+
+	if err = proc.Start(); err != nil {
 		return err
 	}
-	return cmd.Wait()
+
+	return proc.Wait()
 }
 
 // Close is mandatory for the interface, here it does nothing
 func (d ExecDriver) Close() error { return nil }
+
+func renderCommand(line string, tmplArgs RunArgs) (string, error) {
+	tmpl, err := templates.New(line + "\n")
+	if err != nil {
+		return "", err
+	}
+
+	var buff bytes.Buffer
+	if err = tmpl.Execute(&buff, tmplArgs); err != nil {
+		return "", err
+	}
+
+	return buff.String(), nil
+}
+
+func findAvailableShell(shells []string) string {
+	for _, shell := range shells {
+		if isExecutable(shell) {
+			return shell
+		}
+	}
+
+	return ""
+}
+
+func isExecutable(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	// on Windows, the executable bit is not necessary
+	if runtime.GOOS == "windows" {
+		return true
+	}
+
+	// on Linux actually check if the file is executable
+	return info.Mode()&0111 != 0
+}
